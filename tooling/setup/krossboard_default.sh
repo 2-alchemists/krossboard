d87c7f88ac84fcb310067f8a383debe53507ab55
Rodrigue Chakode <rodrigue.chakode@gmail.com>
2021-06-18 23:18:23 +0200

    updated cloud images to v1.2.0

--- tooling/setup/krossboard_default.sh
+++ tooling/setup/krossboard_default.sh
@@ -20,15 +20,7 @@ export KB_AZURE_VM_SIZE_DEFAULT='Standard_B1ms'
 export KB_AZURE_LOCATION_DEFAULT='centralus'
 export KB_AZURE_IMAGE="/subscriptions/${KB_AZURE_PROVIDER_SUB}/resourceGroups/krossboard-release/providers/Microsoft.Compute/galleries/KrossboardRelease/images/Krossboard"
 
-export KB_GCP_IMAGE='krossboard-v20201117t1605645053-09606ec'
+export KB_GCP_IMAGE='krossboard-v20210618t1623996404-4b9d4f8'
 
 declare -A KB_AWS_AMIS
-KB_AWS_AMIS["ap-southeast-1"]='ami-06d89ea0791265b79'
-KB_AWS_AMIS["ap-southeast-2"]='ami-091b07b73f0a666f6'
-KB_AWS_AMIS["ca-central-1"]='ami-0007e0f266ed68af3'
-KB_AWS_AMIS["eu-central-1"]='ami-099401528712d58ad'
-KB_AWS_AMIS["eu-west-1"]='ami-0944ebe2b12f7c709'
-KB_AWS_AMIS["eu-west-2"]='ami-0c62e6fb66033877f'
-KB_AWS_AMIS["sa-east-1"]='ami-09fd54ddd192936c5'
-KB_AWS_AMIS["us-east-1"]='ami-0bab14d5e51cf1613'
-KB_AWS_AMIS["us-west-1"]='ami-09c50ca2ef3ba69d3'
+KB_AWS_AMIS["eu-central-1"]='ami-051f8d90af3a6fbcb'
